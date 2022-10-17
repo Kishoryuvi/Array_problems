1439e97fd6daf7bb7254fc09b32ea5cfa477f844
Yuvraj Kishor <77017763+Kishoryuvi@users.noreply.github.com>
2022-10-17 23:24:05 +0530

    Create reverseAnArray.cpp

---
+++ reverseAnArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+     int n; 
+     cin>>n;
+     int A[n];
+     for(int i=0;i<n;i++)
+     {
+        cin>>A[i];
+     }
+     for(int i=n-1;i>=0;i--)
+     {
+         cout<<A[i]<<" ";
+     }
+     cout<<endl;
+ }
+ return 0;
+}
